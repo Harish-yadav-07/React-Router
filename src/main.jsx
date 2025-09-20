d59751b02e60d09752887d56b04a5fda33dbcd1f
Harish-yadav-07 <hkyadav9034@gmail.com>
2025-09-20 15:00:30 +0530

    Final Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
-// Import ReactDOM for rendering the app
 import ReactDOM from 'react-dom/client'
-// Import global styles
 import './index.css'
-// Import necessary components from react-router-dom
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 // Import custom layout and page components
 import Layout from './Layout.jsx'
@@ -15,17 +12,11 @@ import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 // Create router configuration using route elements
 const router = createBrowserRouter(
   createRoutesFromElements(
-    // Root route with Layout component
     <Route path='/' element={<Layout />}>
-      {/* Home page route */}
       <Route path='' element={<Home />} />
-      {/* About page route */}
       <Route path='about' element={<About />} />
-      {/* Contact page route */}
       <Route path='contact' element={<Contact />} />
-      {/* Dynamic user route */}
       <Route path='user/:userid' element={<User />} />
-      {/* Github page route with data loader */}
       <Route loader={githubInfoLoader} path='github' element={<Github />} />
     </Route>
   )
